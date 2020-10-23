645e5f9f2c28c35ed4cf3f428c5cd86b2ded66b7
Mirai-UwU <69341830+Mirai-UwU@users.noreply.github.com>
2020-10-23 20:27:47 +0800

    done with work
    im done

--- Application/Source/Scene1.h
+++ Application/Source/Scene1.h
@@ -5,6 +5,13 @@
 
 class Scene1 : public Scene
 {
+enum GEOMETRY_TYPE
+{
+	GEO_TRIANGLE_1 = 0,
+	GEO_TRIANGLE_2,
+	NUM_GEOMETRY,
+};
+
 public:
 	Scene1();
 	~Scene1();
@@ -13,6 +20,15 @@ public:
 	virtual void Update(double dt);
 	virtual void Render();
 	virtual void Exit();
+
+private:
+	unsigned m_vertexArrayID;
+	unsigned m_vertexBuffer[NUM_GEOMETRY];
+	unsigned m_colorBuffer[NUM_GEOMETRY];
+	unsigned m_programID;
 };
+
+
+
 
 #endif
