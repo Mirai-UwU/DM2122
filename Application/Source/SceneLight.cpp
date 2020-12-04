899f2e9555161265dc1ba591c88d7e42243adb69
Mirai-UwU <69341830+Mirai-UwU@users.noreply.github.com>
2020-12-04 14:14:04 +0800

    update light

--- Application/Source/SceneLight.cpp
+++ Application/Source/SceneLight.cpp
@@ -67,7 +67,7 @@ void SceneLight::Init()
 
 	light[0].position.Set(0, 20, 0);
 	light[0].color.Set(1, 1, 1);
-	light[0].power = 10;
+	light[0].power = 1;
 	light[0].kC = 1.f;
 	light[0].kL = 0.01f;
 	light[0].kQ = 0.001f;
